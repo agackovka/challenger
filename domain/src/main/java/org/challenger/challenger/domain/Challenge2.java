4d2821c26a9aaf42b752e150365415aaee7fb614
Anton Gatskovka <agackovka@gmail.com>
2022-03-06 17:28:49 +0300

    Attempt to fix maven error

---
+++ domain/src/main/java/org/challenger/challenger/domain/Challenge2.java
@@ -0,0 +1,10 @@
+package org.challenger.challenger.domain;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+public class Challenge2 {
+
+	private final String name;
+
+}
--- infrastructure/src/main/java/org/challenger/challenger/service/ChallengeService.java
+++ infrastructure/src/main/java/org/challenger/challenger/service/ChallengeService.java
@@ -1,9 +1,15 @@
 package org.challenger.challenger.service;
 
+import org.challenger.challenger.domain.Challenge2;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
 
 @Service
 public class ChallengeService {
+	@PostConstruct
 	public void createChallenge() {
+		Challenge2 asdf = new Challenge2("asdf");
+		System.out.println('x');
 	}
 }
