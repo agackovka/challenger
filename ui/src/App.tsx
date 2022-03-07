6062b0f083e3be05bb2d0466541c059dc6267fd8
Pchelinskii <pchelinskii@gmail.com>
2022-03-07 13:50:13 +0300

    Added ant design

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,4 +1,9 @@
 import React, {useEffect, useState} from 'react';
+import 'antd/dist/antd.css';
+import { Typography, Divider } from 'antd';
+import { Card, Progress } from 'antd';
+
+
 import API from './service/API';
 
 
@@ -6,7 +11,7 @@ function App() {
     const [challenge, setChallenge] = useState<any>({})
 
     useEffect(() => {
-        API.get('/api/v1/challenges/1b87bdd5-df75-47be-a8f6-a8b3b4a935d5')
+        API.get('/api/v1/challenges/ca3d68d4-dbb1-4c63-87e3-a2d22f2ac16e')
             .then((res: any) => {
                 setChallenge(res.data)
             });
@@ -15,13 +20,26 @@ function App() {
 
 
     console.log(challenge)
+    const { Title } = Typography;
   return (
     <div className="App">
-      <h1>HI</h1>
+                <Title level={3}>text challenge</Title>
+                <Divider />
+        <Card hoverable title="title challenge"  style={{ width: 300, margin: "0 auto",}}>
+            <p>goal challenge: 300</p>
+            <Progress
+                type="circle"
+                strokeColor={{
+                    '0%': '#108ee9',
+                    '100%': '#87d068',
+                }}
+                percent={75}
+            />
+        </Card>
 
-      <h2>
-          {challenge.name}
-      </h2>
+        <h2>
+            {challenge.name}
+        </h2>
         <h2>
             {challenge.progress}
         </h2>
